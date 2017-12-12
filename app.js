f4d8f258b8b1f40340a09f1a2ef3207413ee94da
Melissa Gu <melissa.gu@richmond.edu>
2017-12-12 00:19:43 +0000

    further changes

--- app.js
+++ app.js
@@ -45,6 +45,38 @@ app.get("/solprep", function(req, res) {
   res.render("solprep");
 })
 
+// Algebra 1 SOL
+app.get("/algebra1", function(req, res) {
+  res.render("algebra1");
+})
+  // Algebra 1 Calculator Strategy Videos
+  app.get("/algebra1calcvids", function(req, res) {
+    res.render("algebra1calcvids");
+  })
+  // Algebra 1 Song Videos
+  app.get("/algebra1songvids", function(req, res) {
+    res.render("algebra1songvids");
+  })
+
+// Geometry SOL
+app.get("/geometry", function(req, res) {
+  res.render("geometry");
+})
+
+  // Geometry Videos
+  app.get("/geometryvids", function(req, res) {
+    res.render("geometryvids");
+  })
+  
+// Algebra 2 SOL
+app.get("/algebra2", function(req, res) {
+  res.render("algebra2");
+})
+  // Algebra 2 Videos
+  app.get("/algebra2vids", function(req, res) {
+    res.render("algebra2vids");
+  })
+
 // Volunteer Page
 app.get("/volunteer", function(req, res) {
   res.render("volunteer");
