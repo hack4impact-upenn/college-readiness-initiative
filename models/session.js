0ae2b3d558fbc3e0977a9e3160cb137ff6e9f9c3
Hammad-H <hammad.hassan@richmond.edu>
2018-10-08 22:59:50 -0400

    Created the session

--- models/session.js
+++ models/session.js
@@ -3,7 +3,11 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/session_db");
 
 var sessionSchema = new mongoose.Schema({
-    date: Date,
-    Student: Student,
-    Tutor: Tutor
+	date: new Date(),
+	Student: new User(),
+	Tutor: new User()
+
 });
+
+var session = mongoose.model("session", sessionSchema);
+
