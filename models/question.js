a8841621113e46d57552d93a9a4d5ff76505033c
hunter-evans <hunter.evans@richmond.edu>
2018-10-10 14:48:08 -0400

    question.js now exports quesiton schema. parse_csv.js now saves documents to database.

--- models/question.js
+++ models/question.js
@@ -21,6 +21,7 @@ var questionSchema = new mongoose.Schema({
 // Compile the questionSchema into a model, returning an object that
 //has methods that allow us to 
 var Question = mongoose.model("Question", questionSchema);
+module.exports = mongoose.model('Question', questionSchema);
 
 // Method that populates database with a fake question
 function addFakeQuestion() {
