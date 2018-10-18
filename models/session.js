7e403536efa7c6c363f67697055327add23ad392
Hammad-H <hammad.hassan@richmond.edu>
2018-10-18 17:26:07 -0400

    Added fake session methods

--- models/session.js
+++ models/session.js
@@ -9,5 +9,35 @@ var sessionSchema = new mongoose.Schema({
 
 });
 
-var session = mongoose.model("session", sessionSchema);
+var Session = mongoose.model("session", sessionSchema);
 
+function addFakeSession() {
+    Session.create({
+        date: new Date('December 17, 1996'),
+        Student: new User("student", "studentPassword"),
+        Tutor: new User("tutor", "tutorPassword")
+    }, function(err, session) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Added session to database");
+        }
+    });
+}
+
+// Method that displays questions in the database
+function viewSessions() {
+    Session.find({}, function(err, sessions){
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Sessions:");
+            console.log(sessions);
+        }
+    });
+}
+
+addFakeSession();
+viewSessions();
