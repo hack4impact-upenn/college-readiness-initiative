951bfa8d966add9d1f572696dec96b6af5b6685e
Victor Chen <vchen8761@gmail.com>
2018-10-29 17:52:14 -0400

    testing admin.js

--- models/admin.js
+++ models/admin.js
@@ -1,6 +1,8 @@
 var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 mongoose.connect("mongodb://localhost:27017/admin_db");
+//var connection = mongoose.createConnection("mongodb://localhost:27017/admin_db");
+//var db = mongoose.connection;
 
 var AdminSchema = new mongoose.Schema({
     username: String,
@@ -10,4 +12,28 @@ var AdminSchema = new mongoose.Schema({
 
 AdminSchema.plugin(passportLocalMongoose);
 var Admin = mongoose.model("Admin", AdminSchema);
+//var Admin = connection.model("Admin", AdminSchema);
 module.exports = Admin;
+
+function removeUser() {
+    Admin.deleteOne({ username: 'username' }, function (err) {
+        if (err) return handleError(err);
+    });
+}
+
+function populateDB() {
+    Admin.create({
+        username: "username",
+        password: "password"
+    }, function (err, question) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Added test");
+        }
+    });
+}
+
+populateDB();
+//removeUser();
