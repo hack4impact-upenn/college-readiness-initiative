944ddfa35bfbab51b6cdff1277a609bac3be8213
Melissa Gu <melissa.gu@richmond.edu>
2018-11-06 20:31:12 -0500

    generate fakes

--- fake_data.js
+++ fake_data.js
@@ -1,41 +1,37 @@
 var mongoose = require('mongoose');
 var faker = require('faker');
 var Student = require('./models/student.js');
+var Tutor = require('./models/tutor.js');
+var Admin = require('./models/admin.js');
+var Session = require('./models/session.js');
 var School = require('./models/school.js');
 mongoose.connect('mongodb://localhost:27017/college_readiness_initiative', { useNewUrlParser: true });
 
+// Generates 4 schools
 function addFakeSchools() {
     var schools = ["John Marshall High School", "Huguenot High School",
                    "Armstrong High School", "Thomas Jefferson High School"];
     schools.forEach(school => {
         School.create({
             name: school
-        }, function(err, school){
-            console.log(err);
-            if (err) {
-                console.log(err);
-            }
-            else {
-                console.log("Added school: " + school);
-            }
         });
     });
 }
 
+// Generates 10 fake students
 function addFakeStudents() {
-    for (var i = 0; i < 4; i++) {
+    for (var i = 0; i < 10; i++) {
         var random_school = School.findOne({}, function (err, res) {
-            console.log("School:" + res);
-        });
+            console.log("School:" + res.name);
+        }).name;
         var years = [2018, 2019, 2020, 2021];
         var random_year = years[Math.floor(Math.random() * years.length)];
         var random_sat_score = Math.floor(Math.random() * 1600);
-        console.log("Score: " + random_sat_score);
         var random_num_questions_completed = Math.floor(Math.random() * 100);
         Student.create({
             username: faker.internet.userName(),
             password: faker.internet.password(),
-            name: faker.internet.password(),
+            name: faker.name.findName(),
             school: random_school,
             year: random_year,
             past_sat_score: random_sat_score,
@@ -50,11 +46,67 @@ function addFakeStudents() {
         });
     }
 }
-addFakeSchools(function(err) {
-    if (err) {
-        console.log(err);
-    } else {
-        addFakeStudents();
+
+// Generates 5 fake tutors
+function addFakeTutors() {
+    for (var i = 0; i < 5; i++) {
+        var random_tutee = Student.findOne({}).name;
+        Tutor.create({
+            username: faker.internet.userName(),
+            password: faker.internet.password(),
+            name: faker.name.findName(),
+            tutee_username: random_tutee
+        });
     }
-});
+}
+
+// Generates admin account
+function addAdminAccount() {
+    Admin.create({
+        username: "admin",
+        password: "password"
+    }, function(err, admin) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(admin);
+        }
+    });
+}
+
+// Generates 10 tutoring sessions
+function addFakeSessions() {    
+    for (var i = 0; i < 10; i++) {
+        var randomStudent = Student.findOne({}, function(err, randomStudent) {
+            var randomTutor = Tutor.findOne({}, function(err, randomTutor) {
+                Session.create({
+                    date: Date.now(),
+                    student: randomStudent._id,
+                    tutor: randomTutor._id,
+                }, function (err, session) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(session);
+                        console.log("Student: " + session.student.toString());
+                        console.log("Tutor: " + session.tutor.toString());
+                    }
+                });
+            });
+        });
+    }
+}
+// addFakeSchools(function(err) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         addFakeStudents();
+//     }
+// });
+// addFakeStudents();
+// addFakeTutors();
+// addAdminAccount();
+addFakeSessions();
+
+
 
--- models/session.js
+++ models/session.js
@@ -50,5 +50,5 @@ function viewSessions() {
     });
 }
 
-addFakeSession();
-viewSessions();
+// addFakeSession();
+// viewSessions();
