1b4a760ca9456f1105c1feb470b478ebae43c15f
hunter-evans <hunter.evans@richmond.edu>
2018-11-19 23:22:15 -0500

    Really tried, but reaching dead ends. Probably going to start fresh.

--- scripts/insertStudentQuestions.js
+++ scripts/insertStudentQuestions.js
@@ -2,25 +2,50 @@ var mongoose = require('mongoose');
 var QuestionModel = require('../models/question');
 var StudentModel = require('../models/student');
 
-var insertStudentQuestions = function insertStudentQuestions(student) {
-	QuestionModel.find().distinct('type').then(function(questionTypes) {
-		questionTypes.forEach(function(questionType) { 
-			if(!(questionType in student.current_questions)) {
-				student.current_questions[questionType] = [];
-			}
-			QuestionModel.find({type: questionType}).then(function(questions) {
-				questions.forEach(function(question) {
-					student.current_questions[questionType].push(question._id);
-					//console.log("Student's current questions:" + JSON.stringify(student.current_questions[questionType]));
-				});
-			}).then(function() {
-				student.save(function(err) {
-					if (err) console.log(err);
-					console.log("saving student");
-				});	
-			});
-		});
-	});
+var insertStudentQuestions = function insertStudentQuestions() {
+//		QuestionModel.find().distinct('type').then( function(questionTypes) {
+//			for(var i = 0; i < questionTypes.length; i++) {
+//				var type = questionTypes[i];
+//				StudentModel.findOne({}).then( function(student) {
+//					console.log("type: " + type);
+//					var temp = Object.assign({[type]: []}, student.current_questions);
+//					console.log("temp: " + JSON.stringify(temp));
+//					return [student, temp];
+//				}).then( function(arr) {
+//					return Object.assign(arr[0], {current_questions: arr[1]});
+//				}).then( function(student) {
+//					return student.save();
+//				}).then( function(student) {
+//					console.log(student + " saved");
+//				});
+//			}
+//		}); 
+//	QuestionModel.find().distinct('type').then(function(questionTypes) {
+//		questionTypes.forEach(function(questionType) { 
+//			if(!(questionType in student.current_questions)) {
+//				student.current_questions[questionType] = [];
+//				console.log("inserted " + questionType + " questions");
+//			}
+//		})
+//		student.save();
+//		console.log("finished");});
+//	}).then( function(student) { 
+//		student.save();
+//	});
+//			QuestionModel.find({type: questionType}).then(function(questions) {
+//				questions.forEach(function(question) {
+//					student.current_questions[questionType].push(question._id);
+//					//console.log("Student's current questions:" + JSON.stringify(student.current_questions[questionType]));
+//				});
+//				return student;
+//			}).then(function(student) {
+//				student.save(function(err) {
+//					if (err) console.log(err);
+//					console.log("saving student");
+//				});	
+//			});
+//		});
+//	});
 }
 
 module.exports = insertStudentQuestions;
